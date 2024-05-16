239ca537f935aed47631ee2a9b54d82384385d0d
William Tan <esaiaswt@gmail.com>
2024-05-16 11:07:39 +0800

    include torch is requirements

--- app.py
+++ app.py
@@ -63,8 +63,7 @@ if uploaded_file is not None:
    # Download stable diffusion model from hugging face
    modelid = "CompVis/stable-diffusion-v1-4"
    device = "cuda"
-   st.write(HF_TOKEN_KEY)
-   stable_diffusion_model = StableDiffusionPipeline.from_pretrained(modelid, revision="fp16", torch_dtype=torch.float16, use_auth_token=HF_TOKEN_KEY, silence_dtype_warnings = True)
+   stable_diffusion_model = StableDiffusionPipeline.from_pretrained(modelid, revision="fp16", torch_dtype=torch.float16, use_auth_token=HF_TOKEN_KEY)
    stable_diffusion_model.to(device)
 
    save_name = ""
