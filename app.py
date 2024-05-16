688f08c433fdd41a24732c238924f8a44b386b16
William Tan <esaiaswt@gmail.com>
2024-05-16 11:12:25 +0800

    debug verbose for StableDiffusionPipeline

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ if uploaded_file is not None:
    # Download stable diffusion model from hugging face
    modelid = "CompVis/stable-diffusion-v1-4"
    device = "cuda"
-   stable_diffusion_model = StableDiffusionPipeline.from_pretrained(modelid, revision="fp16", torch_dtype=torch.float16, use_auth_token=HF_TOKEN_KEY)
+   stable_diffusion_model = StableDiffusionPipeline.from_pretrained(modelid, revision="fp16", torch_dtype=torch.float16, use_auth_token=HF_TOKEN_KEY, verbose = False)
    stable_diffusion_model.to(device)
 
    save_name = ""
